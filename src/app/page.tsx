67a39e058e6bfab727799e813836c1d1208145f3
Rony Kati <73883742+ronykax@users.noreply.github.com>
2025-06-30 00:53:54 +0530

    list items in home - use inter - update README.md

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,47 @@
+"use client";
+
 export default function Home() {
+    const Item = ({
+        name,
+        url,
+        className,
+    }: {
+        name: string;
+        url: string;
+        className: string;
+    }) => {
+        return (
+            <button
+                className={`cursor-pointer duration-100 ${className}`}
+                onClick={() => (window.location.href = url)}
+            >
+                <span className="block md:-rotate-45">{name}</span>
+            </button>
+        );
+    };
+
     return (
-        <div>
-            <span className="text-2xl">hi world!!!!</span>
+        <div className="grid grid-cols-1 md:grid-cols-4 w-full h-screen text-2xl font-bold">
+            <Item
+                name="pomodoro"
+                url="/pomodoro"
+                className="bg-red-400/25 hover:bg-red-400/50"
+            />
+            <Item
+                name="notes"
+                url="/notes"
+                className="bg-yellow-300/25 hover:bg-yellow-300/50"
+            />
+            <Item
+                name="todo"
+                url="/todo"
+                className="bg-blue-400/25 hover:bg-blue-400/50"
+            />
+            <Item
+                name="reminders"
+                url="/reminders"
+                className="bg-purple-500/25 hover:bg-purple-500/50"
+            />
         </div>
-    )
+    );
 }
